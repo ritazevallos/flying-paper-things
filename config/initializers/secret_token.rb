29b63b5ca117c2be973306b89f00eb0474af62d3
Rita Zevallos <rzevall1@swarthmore.edu>
2014-07-25 18:13:13 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlyingPaperThings::Application.config.secret_key_base = 'c8aa7a824252e005050b70024a557b274e4a8afc21d5a0ce2d83c9a930429071d2fc611d2c882bf268e51e480d504c5ec9e5ff969b8bd915dea61052aa2be203'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlyingPaperThings::Application.config.session_store :cookie_store, key: '_flying-paper-things_session'
